76173522e2c9a9e8e4831da7e9acdff9b07000af
Romilda Bode <romildab@ob-i5.nsk.21-school.ru>
2022-08-10 21:23:54 +0700

    need fix whether icon

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
 import { Input } from './input/Index.js';
+import Card from './weathercard/wecard';
 
 function App() {
   return (
     <div className="Main">
       <Input />
+      <div className='CardList'>
+        <Card />
+      </div>
     </div>
   );
 }
---
+++ weather-app/src/weathercard/wecard.js
@@ -0,0 +1,11 @@
+import '../App.css'
+
+function Card() {
+    return (
+        <div className='WeCard'>
+
+        </div>
+    );
+}
+
+export default Card;
