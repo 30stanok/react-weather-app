be18bcce57b1a4f35ffe05293702bee21b8301c4
Romilda Bode <romildab@ob-i5.nsk.21-school.ru>
2022-08-10 19:19:52 +0700

    test import/export

---
+++ weather-app/src/input/Index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import '../App.css';
+
+function Input() {
+    return (
+        <div className="header">
+            <p>Input</p>
+        </div>
+    );
+}
+
+function Ios() {
+    return (
+        <div className="header">
+            <p>pivo</p>
+        </div>
+    );
+}
+
+export {Input, Ios};
