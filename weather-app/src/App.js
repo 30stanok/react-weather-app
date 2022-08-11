b3accb1a266ebf302db4f0f86bd9ec19dfed7d58
Romilda Bode <romildab@si-d4.nsk.21-school.ru>
2022-08-11 18:13:06 +0700

    need fix props in App

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 
-import { Input } from './input/Index.js';
+import { Input } from './input/inp.js';
 import Card from './weathercard/wecard';
 
 function App() {
+  console.log("App load");
+  const citiesList = ["nsk", "msk", "jdanovo"];
   return (
     <div className="Main">
       <Input />
       <div className='CardList'>
-        <Card />
+        {
+          citiesList.map(city => {
+            <Card city={city}/>
+          })
+        }
       </div>
     </div>
   );
--- weather-app/src/weathercard/wecard.js
+++ weather-app/src/weathercard/wecard.js
@@ -1,9 +1,19 @@
 import '../App.css'
 
-function Card() {
+function Card(props) {
+    console.log(props);
     return (
         <div className='WeCard'>
-
+            <div className='MainInfo'>
+                <img className='Icon' src='http://openweathermap.org/img/wn/10d@2x.png' alt='Icon'></img>
+                <div className='Title'>{props.city}</div>
+                <div className='Discription'>Cloudy</div>
+                <div className='Timperature'>20</div>
+            </div>
+            <div className='Information'>
+                <div>Humidity: 15</div>
+                <div>Feels like: 19</div>
+            </div>
         </div>
     );
 }
